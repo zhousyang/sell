8cf855d463c5b74face036b766f6e24574965882
zhouyang <young_zy1116@163.com>
2018-12-12 09:10:58 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="app">
+		<div class="header" @click='devClick'>
+			 I am header
+		</div>
+		<chart></chart>
+	</div>
+</template>
+
+<script>
+	import chart from './components/charts'
+	export default{
+		 data(){
+			  return{
+					
+				}
+		 },
+		 components:{
+			 chart:chart
+		 },
+		 methods:{
+			 devClick:function(){
+				  $('.header').addClass('turnRed');
+			 }
+		 }
+	}
+</script>
+
+<style>
+	 .turnRed{
+		  background:red;
+	 }
+</style>
---
+++ src/components/charts.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		 <div id="chart" class="chart">
+
+         </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				myChart:{}
+			};
+		},
+		mounted(){
+			this.myChart=this.$echarts.init(document.getElementById('chart'));
+			this.drawChart();
+		},
+		methods:{
+			drawChart(){
+				let option = {
+                  xAxis: {
+                      type: 'category',
+                      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                  },
+                  yAxis: {
+                      type: 'value'
+                  },
+                  series: [{
+                      data: [120, 200, 150, 80, 70, 110, 130],
+                      type: 'bar'
+                  }]
+                };
+
+                this.myChart.setOption(option);
+                window.onresize = function(){
+                       this.myChart.resize()
+                }
+			}
+		}
+	}
+</script>
+
+<style>
+   .chart{
+	   width:200px;
+	   height:150px;
+   }
+</style>
